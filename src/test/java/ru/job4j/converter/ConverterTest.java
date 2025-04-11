9ed406b51f359f7032f43fce191a4fc3d7596e6f
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-11 12:07:17 +0300

    1. Модульные тесты [#505277]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -2,7 +2,7 @@ package ru.job4j.converter;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class ConverterTest {
 
