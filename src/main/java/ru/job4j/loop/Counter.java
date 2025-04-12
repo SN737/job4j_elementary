a378802d8ae84a9afcc8c169496c768973085fb3
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-12 19:25:54 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -15,11 +15,35 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        if (start <= finish) {
+            for (; start <= finish; start++) {
+                if (start % 2 == 0) {
+                    sum = sum + start;
+                }
+            }
+        } else {
+            for (; start >= finish; start--) {
+            if (start % 2 == 0) {
+                sum = sum + start;
+                }
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
         System.out.println(sum(13, 10));
         System.out.println(sum(10, 10));
+
+        System.out.println("evenSum " + sumByEven(0, 10));
+        System.out.println("evenSum " + sumByEven(3, 8));
+        System.out.println("evenSum " + sumByEven(1, 2));
+        System.out.println("evenSum " + sumByEven(4, 2));
+        System.out.println("evenSum " + sumByEven(-4, 2));
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,10 +1,12 @@
 package ru.job4j.loop;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withPrecision;
 import static ru.job4j.loop.Counter.sum;
+import static ru.job4j.loop.Counter.sumByEven;
 
 class CounterTest {
 
@@ -32,6 +34,26 @@ class CounterTest {
         int finish = 10;
         double sum = sum(start, finish);
         double expected = 10;
+        assertThat(sum).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    @DisplayName("sumByEvenTest")
+    void whenStart0Finish10Then30() {
+        int start = 0;
+        int finish = 10;
+        double sum = sumByEven(start, finish);
+        double expected = 30;
+        assertThat(sum).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    @DisplayName("sumByEvenTest")
+    void whenStartMinus4Finish2ThenMinus4() {
+        int start = -4;
+        int finish = 2;
+        double sum = sumByEven(start, finish);
+        double expected = -4;
         assertThat(sum).isEqualTo(expected, withPrecision(0.01));
     }
 }
