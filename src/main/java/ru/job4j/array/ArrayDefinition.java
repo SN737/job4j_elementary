2945ca06da9b4b5ce42a93621a2637e8bc4512a0
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-14 13:15:37 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,19 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String [] names = new String[4];
+
+        names[0] = "Quinn";
+        names[1] = "Jack";
+        names[2] = "Luke";
+        names[3] = "Connor";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        for(int i = 0; i <names.length; i++) {
+        System.out.println(names[i]);
+        }
     }
 }
