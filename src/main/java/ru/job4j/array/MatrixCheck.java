496d0c248290fe56ada7ded274239430ea6b4284
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-16 15:36:13 +0300

    6.7.1. Моно столбец без указания столбца

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,4 +11,23 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[j][i] == 'X') {
+                    result = true;
+                } else {
+                    result = false;
+                    break;
+                }
+            }
+                if (result) {
+                    break;
+                }
+
+        }
+        return result;
+    }
 }
